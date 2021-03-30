0aa9f7d1049ba32f061c774d36f1024b1cac0379
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-03-30 23:05:24 +0700

    basic 01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React, { Fragment, useRef, useEffect, useState } from 'react';
+import { select } from 'd3';
+
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([25, 30, 45, 60, 20]);
+
+  const svgRef = useRef();
+
+  useEffect(() => {
+    const svgSelect = select(svgRef.current);
+    svgSelect
+      .selectAll('circle')
+      .data(data)
+      .join('circle')
+      .attr('r', value => value)
+      .attr('cx', value => value * 2)
+      .attr('cy', value => value * 2)
+      .attr('stroke', 'red');
+  }, [data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <svg ref={svgRef}></svg>
+      <br />
+      <button onClick={() => setData(data.map(value => value + 5))}>Update data</button>
+    </Fragment>
   );
 }
 
